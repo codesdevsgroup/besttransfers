83e70d4572c7c65645fd846e8bd4dc859978cbbe
fabianosantos2323 <fabianojunior77@gmail.com>
2023-07-13 19:47:39 -0300

    fix: spaces and styles

--- assets/js/translate.js
+++ assets/js/translate.js
@@ -10,10 +10,25 @@ function translate() {
     const language =
         localStorage.getItem('@besttransfer-1.0.0-language') ?? 'en'
     document.querySelector('#navigation .form-select').value = language
-    if (language === 'en') {
-        document.getElementById('en-flag').style = 'display: block !important'
-    } else {
-        document.getElementById('pt-flag').style = 'display: block !important'
+    switch (language) {
+        case 'en':
+            document.getElementById('en-flag').style =
+                'display: block !important'
+            break
+        case 'pt':
+            document.getElementById('pt-flag').style =
+                'display: block !important'
+            break
+        case 'fr':
+            document.getElementById('fr-flag').style =
+                'display: block !important'
+            break
+        case 'es':
+            document.getElementById('es-flag').style =
+                'display: block !important'
+            break
+        default:
+            break
     }
     translateNavigation(language)
     if (document.querySelector('#home') !== null) {
